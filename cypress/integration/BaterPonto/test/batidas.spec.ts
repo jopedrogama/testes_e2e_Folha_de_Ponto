ca72adec589e9a4292c72c920acc8a98a454148e
João Pedro Gama F. Mattos <jopedrogama@gmail.com>
2022-08-17 15:27:51 -0300

    Testes atualizados

--- cypress/integration/BaterPonto/test/batidas.spec.ts
+++ cypress/integration/BaterPonto/test/batidas.spec.ts
@@ -12,7 +12,7 @@ describe('Registrar Horarios', () => {
     batidaParams.dataHora = `${dia}T${horario}`;
 
     new CadastrarBatida().inserirPonto(batidaParams).then((response)=>{
-      expect(response.status).to.be.equal(200);
+      expect(response.status).to.be.equal(201);
       expect(response.body.dia).to.be.equal(dia);
       expect(response.body.horarios.length).to.be.equal(1);
 
@@ -27,11 +27,25 @@ describe('Registrar Horarios', () => {
     batidaParams.dataHora = `${dia}T${horario}`;
 
     new CadastrarBatida().inserirPonto(batidaParams).then((response)=>{
-      expect(response.status).to.be.equal(200);
+      expect(response.status).to.be.equal(201);
       expect(response.body.dia).to.be.equal(dia);
       expect(response.body.horarios.length).to.be.equal(2);
 
     })
+  })
+
+  it('Batida 3 - Menos de uma hora de almoço', ()=>{
+    let dia: string = "2018-09-21"
+    let horario: string = "12:15:10";
+    let batidaParams: BatidaParams = new BatidaParams();
+    batidaParams.dataHora = `${dia}T${horario}`;
+
+    new CadastrarBatida().inserirPonto(batidaParams).then((response)=>{
+      expect(response.status).to.be.equal(403);
+      expect(response.body.mensagem).to.be.equal("Deve haver no mínimo 1 hora de almoço");
+
+    })
+
   })
 
   it('Batida 3', ()=>{
@@ -41,7 +55,7 @@ describe('Registrar Horarios', () => {
     batidaParams.dataHora = `${dia}T${horario}`;
 
     new CadastrarBatida().inserirPonto(batidaParams).then((response)=>{
-      expect(response.status).to.be.equal(200);
+      expect(response.status).to.be.equal(201);
       expect(response.body.dia).to.be.equal(dia);
       expect(response.body.horarios.length).to.be.equal(3);
 
@@ -56,14 +70,14 @@ describe('Registrar Horarios', () => {
     batidaParams.dataHora = `${dia}T${horario}`;
 
     new CadastrarBatida().inserirPonto(batidaParams).then((response)=>{
-      expect(response.status).to.be.equal(200);
+      expect(response.status).to.be.equal(201);
       expect(response.body.dia).to.be.equal(dia);
       expect(response.body.horarios.length).to.be.equal(4);
 
     })
   })
 
-  it('Batida 5', ()=>{
+  it('Batida 5 - Só deve registrar 4 por dia', ()=>{
     let dia: string = "2018-09-21"
     let horario: string = "20:21:32";
     let batidaParams: BatidaParams = new BatidaParams();
@@ -72,6 +86,18 @@ describe('Registrar Horarios', () => {
     new CadastrarBatida().inserirPonto(batidaParams).then((response)=>{
       expect(response.status).to.be.equal(403);
       expect(response.body.mensagem).to.be.equal("Apenas 4 horários podem ser registrados por dia");
+    })
+  })
+
+  it('Não pode fazer ponto no final de semana', ()=>{
+    let dia: string = "2022-08-13"
+    let horario: string = "08:21:32";
+    let batidaParams: BatidaParams = new BatidaParams();
+    batidaParams.dataHora = `${dia}T${horario}`;
+
+    new CadastrarBatida().inserirPonto(batidaParams).then((response)=>{
+      expect(response.status).to.be.equal(403);
+      expect(response.body.mensagem).to.be.equal("Sábado e domingo não são permitidos como dia de trabalho");
     })
   })
 
